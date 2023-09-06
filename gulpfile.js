79ccb73521f24cdd7dca177ecab69de4c0e615f7
Kamil <65686983+jastrzembowski@users.noreply.github.com>
2023-09-06 14:39:46 +0200

    css purged

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("supercss/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("supercss/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
